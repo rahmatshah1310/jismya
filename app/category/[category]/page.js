5f593a6c0705df1d32f059907863dd0ae6dafce0
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-25 14:09:39 +0500

    add

---
+++ app/category/[category]/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { ProductGrid } from "../../../components/products/product-grid";
+
+export default function CategoryPage() {
+  const { category } = useParams();
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ProductGrid categoryId={category} />
+    </div>
+  );
+}
