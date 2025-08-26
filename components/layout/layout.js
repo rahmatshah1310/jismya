0f77a1b8be4d0c5f35f8f6acc9dc7cad5cc26f40
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-26 21:04:19 +0500

    add

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,16 @@
 "use client";
 import { Header } from "./header";
 import { Footer } from "./footer";
+import { Suspense } from "react";
+import { BeatLoader } from "react-spinners";
 
 export function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Header />
-      <main className="flex-1">{children}</main>
+      <Suspense fallback={<BeatLoader color="darkBlue" />}>
+        <main className="flex-1">{children}</main>
+      </Suspense>
       <Footer />
     </div>
   );
