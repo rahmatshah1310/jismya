a7ea80e94a54d344f244cecda4ff2ae7338499dd
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-28 00:04:38 +0500

    update

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -6,10 +6,12 @@ import { BeatLoader } from "react-spinners";
 
 export function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col bg-white">
+    <div className="min-h-screen flex flex-col bg-cream dark:bg-d-bg text-ink dark:text-d-ink">
       <Header />
       <Suspense fallback={<BeatLoader color="darkBlue" />}>
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 animate-fade-in">
+          {children}
+        </main>
       </Suspense>
       <Footer />
     </div>
