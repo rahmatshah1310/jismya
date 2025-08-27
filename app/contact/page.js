ab0a2dfed94963da37834fdd43059e15ef86bef0
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-28 00:03:18 +0500

    update

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,4 +1,4 @@
-import { ContactForm } from "./contact-form";
+import ContactForm from "./contact-form";
 
 export const metadata = {
   title: "Contact Us | User.pk",
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function ContactPage() {
   return (
-    <div className="container-custom py-20">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <ContactForm />
     </div>
   );
