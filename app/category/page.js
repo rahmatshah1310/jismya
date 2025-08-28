3ead1e03b62738b8012bc8387ae07c2813cb1465
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-29 04:18:22 +0500

    add

--- app/category/page.js
+++ app/category/page.js
@@ -39,7 +39,7 @@ export default function CategoriesPage() {
   }
 
   return (
-    <div className="container-custom py-8">
+    <div className="container-custom py-8 sm:px-14 px-2">
       <h1 className="text-3xl font-bold text-gray-800 mb-8">All Categories</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
