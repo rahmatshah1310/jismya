4ee2ce6df6499d5c7696409fcf0a175307ad64fd
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-31 14:23:35 +0500

    add

--- components/ui/skeleton.js
+++ components/ui/skeleton.js
@@ -1,15 +1,7 @@
-import { cn } from "../../lib/utils"
+import { cn } from "@/lib/utils";
 
-function Skeleton({
-  className,
-  ...props
-}) {
-  return (
-    <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
-    />
-  )
+function Skeleton({ className, ...props }) {
+  return <div className={cn("animate-pulse rounded-md bg-gray-300", className)} {...props} />;
 }
 
-export { Skeleton }
+export { Skeleton };
