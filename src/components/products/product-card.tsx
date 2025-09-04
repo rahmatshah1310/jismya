3516eab13c16673bb6c8fe07ecc7e86eedf5125d
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-09-04 15:24:10 +0500

    add

--- src/components/products/product-card.tsx
+++ src/components/products/product-card.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useCart } from "@/app/context/CartContext";
+import { ShoppingCart } from "lucide-react";
 
 interface ProductCardProps {
   product: {
@@ -89,14 +90,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                   ></path>
                 </svg>
               ) : (
-                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-2.5 5M7 13l2.5 5m6-5v6a2 2 0 01-2 2H9a2 2 0 01-2-2v-6m8 0V9a2 2 0 00-2-2H9a2 2 0 00-2 2v4.01"
-                  />
-                </svg>
+                <ShoppingCart />
               )}
             </button>
 
