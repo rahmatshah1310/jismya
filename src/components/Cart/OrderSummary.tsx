652a1170614bc66b6f760c367b677a22f87af670
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-09-05 13:58:34 +0500

    add

--- src/components/Cart/OrderSummary.tsx
+++ src/components/Cart/OrderSummary.tsx
@@ -29,7 +29,7 @@ const OrderSummary = () => {
           {cartItems.map((item, key) => (
             <div key={key} className="flex items-center justify-between py-5 border-b border-gray-3">
               <div>
-                <p className="text-dark">{item.productName}</p>
+                <p className="text-dark">{item.name}</p>
               </div>
               <div>
                 <p className="text-dark text-right">
