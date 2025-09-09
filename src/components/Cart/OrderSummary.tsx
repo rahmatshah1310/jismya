574e344304627c8b5d8b03bd32513534855074aa
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-09-10 00:39:36 +0500

    update

--- src/components/Cart/OrderSummary.tsx
+++ src/components/Cart/OrderSummary.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 
 const OrderSummary = () => {
   const { cart: cartItems, getCartTotal } = useCart();
-  console.log(cartItems[0]);
   const totalPrice = getCartTotal();
   const router = useRouter();
   return (
